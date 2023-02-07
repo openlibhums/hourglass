1024ada8622094e5b0bd39a2e95ecbb16465436e
Joseph Muller <j.muller@bbk.ac.uk>
2023-02-07 17:17:09 +0000

    Experimental implementation of lunr.js

--- build_assets.py
+++ build_assets.py
@@ -5,14 +5,30 @@ from django.conf import settings
 from django.core.management import call_command
 
 THEME_NAME = 'path'
-THEME_DIST_PATH = 'assets'
+
+THEME_ASSET_PATH = os.path.join(settings.BASE_DIR, 'themes', THEME_NAME, 'assets')
+
+OTHER_SOURCE_PATHS = [
+    os.path.join(
+        settings.BASE_DIR, 'themes', THEME_NAME, 'node_modules', 'lunr', 'lunr.min.js'
+    ),
+    os.path.join(
+        settings.BASE_DIR, 'themes', THEME_NAME, 'customLunr.js'
+    ),
+]
+
+JANEWAY_STATIC_PATH = os.path.join(settings.BASE_DIR, 'static', THEME_NAME)
+
+
+def collect_assets():
+    for path in OTHER_SOURCE_PATHS:
+        shutil.copy(path, JANEWAY_STATIC_PATH)
 
 
 def build():
-    src_path = os.path.join(settings.BASE_DIR, 'themes', THEME_NAME, THEME_DIST_PATH)
-    dest_path = os.path.join(settings.BASE_DIR, 'static', THEME_NAME)
     print(f"Copying files for {THEME_NAME} theme:")
-    print(f"From: {src_path}")
-    print(f"To: {dest_path}")
-    shutil.copytree(src_path, dest_path, dirs_exist_ok=True)
+    print(f"Collecting assets in {THEME_ASSET_PATH}")
+    collect_assets()
+    print(f"Copying assets to {JANEWAY_STATIC_PATH}")
+    shutil.copytree(THEME_ASSET_PATH, JANEWAY_STATIC_PATH, dirs_exist_ok=True)
     call_command('collectstatic', '--noinput')
---
+++ customLunr.js
@@ -0,0 +1,109 @@
+async function init(searchInputId = 'search') {
+	const searchInput = document.getElementById(searchInputId);
+	const { documents, searchIndex } = await makeSearchIndex(searchInput);
+	searchInput.addEventListener(
+		"change",
+		event => updateResultsList(event, documents, searchIndex)
+	);
+}
+
+
+async function makeSearchIndex(searchInput) {
+	const documentString = searchInput.getAttribute('data-site-search');
+	const documents = await JSON.parse(documentString);
+	const searchIndex = lunr(function () {
+		this.ref('url');
+		this.field('name');
+		this.field('text');
+		this.metadataWhitelist = ['position'];
+		for (let key in documents) {
+			this.add(documents[key]);
+		}
+	});
+	return { documents, searchIndex };
+}
+
+
+async function updateResultsList(event, documents, searchIndex) {
+	const searchResults = await searchIndex.search(event.target.value)
+	const resultsHTML = document.getElementById('results');
+	resultsHTML.innerHTML = '';
+	for (let result of searchResults) {
+		let doc = documents[result.ref];
+		let mergedPositions = getPositionsByField(result);
+		let li = makeResultListItem(doc, result.ref, mergedPositions);
+		resultsHTML.appendChild(li);
+	}
+}
+
+
+function getPositionsByField(result){
+	const fieldMap = new Map();
+	let metadata = result.matchData.metadata;
+	for (let stem in metadata) {
+		for (let field in metadata[stem]) {
+			if (!fieldMap.has(field)) {
+				fieldMap.set(field, []);
+			}
+			for (let position of metadata[stem][field].position) {
+				fieldMap.get(field).push(position);
+			}
+		}
+	}
+	for (let positionArray of fieldMap.values()) {
+		positionArray.sort((a, b) => {return a[0] - b[0]});
+	}
+	return fieldMap;
+}
+
+
+function makeResultListItem(doc, ref, positions) {
+	let h2 = document.createElement('h2');
+  h2.innerHTML = highlightFieldText(doc, 'name', positions);
+	let p = document.createElement('p');
+  p.innerHTML = highlightFieldText(doc, 'text', positions);
+	let a = document.createElement('a');
+	a.href = ref;
+	a.appendChild(h2);
+	a.appendChild(p);
+	let li = document.createElement('li');
+	li.appendChild(a);
+	return li;
+}
+
+
+function highlightFieldText(doc, field, positions, charLimit=200) {
+	// Thanks to Aaron Taylor for lots of the logic behind this function:
+	// https://github.com/kujenga/website/blob/1768cf384d54b7e7ec8c88a02dc0ec3819061fc5/assets/js/search.jsx#L41-L79
+	if (!positions.size) {
+		return doc[field];
+	}
+	let innerHTML = '';
+	let current = 0;
+	if (positions.has(field)) {
+		let firstPosition = positions.get(field)[0][0];
+		if (firstPosition > charLimit - 20) {
+			innerHTML += '...';
+			current = firstPosition - 20;
+			charLimit += current;
+		}
+		for (let [from, length] of positions.get(field)) {
+			if (from > charLimit) {
+				innerHTML += doc[field].slice(current, charLimit) + '...';
+				break;
+			}
+			let to = from + length;
+			innerHTML += doc[field].slice(current, from);
+			let highlighted = doc[field].slice(from, to);
+			innerHTML += `<span class="highlight">${highlighted}</span>`;
+			current = to;
+		}
+	}
+	innerHTML += doc[field].slice(current, charLimit);
+	if (doc[field].length > charLimit) {
+		 innerHTML += '...';
+	}
+	return innerHTML;
+}
+
+init();
