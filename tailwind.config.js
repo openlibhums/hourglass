62a21b9a0257c5922cf84c4573238189ad153f8a
Joseph Muller <j.muller@bbk.ac.uk>
2023-06-21 15:44:06 +0100

    Add proper colors #29

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,24 @@ export default {
     './templates/**/*.html'
   ],
   theme: {
+    colors: {
+      'hourglass-blue': '#1579A0',
+      'hourglass-orange': '#EC8323',
+      'hourglass-red': '#E94C33',
+      'hourglass-tan': '#D6CBBC',
+      'hourglass-tan-light': '#EAE5DD',
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      black: '#000000'
+    },
     container: {
       padding: {
         DEFAULT: '3rem',
-        sm: '6rem',
-        lg: '8rem',
-        xl: '10rem',
-        '2xl': '12rem'
+        sm: '4rem',
+        lg: '6rem',
+        xl: '8rem',
+        '2xl': '10rem'
       }
     },
     extend: {
