095fd1b345ea7e4ed1661f4de6f7c67bae52ae57
Joseph Muller <j.muller@bbk.ac.uk>
2023-09-28 11:34:29 +0100

    Refine form components #90

--- components/form/form.py
+++ components/form/form.py
@@ -8,30 +8,33 @@ class Form(component.Component):
     template_name = "form/form.html"
 
     def get_context_data(self, *args, **kwargs):
-        form_method = kwargs.pop('form_method')
-        form = kwargs.pop('form')
+        form = kwargs.pop('form', None)
+        form_method = kwargs.pop('form_method', None)
+        enctype = kwargs.pop('enctype', None)
         context = super().get_context_data(*args, **kwargs)
-        for field_name, field in form.fields.items():
-            tailwind = '''
-                bg-transparent border-white
-                text-white cursor-white placeholder:text-white
-                max-lg:placeholder:text-md lg:placeholder:text-lg
-                max-lg:text-md lg:text-lg
-                focus:ring-transparent focus:border-orange
-            '''
-            if isinstance(field.widget, widgets.Select):
-                form.fields[field_name].widget.attrs['class'] = tailwind + '''
-                    border-t-0 border-r-0 border-b-1 border-l-0
+        if form:
+            for field_name, field in form.fields.items():
+                tailwind = '''
+                    bg-transparent border-white
+                    text-white cursor-white placeholder:text-white
+                    max-lg:placeholder:text-md lg:placeholder:text-lg
+                    max-lg:text-md lg:text-lg
+                    focus:ring-transparent focus:border-orange
                 '''
-            elif isinstance(field.widget, widgets.Textarea):
-                form.fields[field_name].widget.attrs['class'] = tailwind + '''
-                    border-1
-                '''
-            else:
-                form.fields[field_name].widget.attrs['class'] = tailwind + '''
-                    border-t-0 border-r-0 border-b-1 border-l-0
-                '''
+                if isinstance(field.widget, widgets.Select):
+                    form.fields[field_name].widget.attrs['class'] = tailwind + '''
+                        border-t-0 border-r-0 border-b-1 border-l-0
+                    '''
+                elif isinstance(field.widget, widgets.Textarea):
+                    form.fields[field_name].widget.attrs['class'] = tailwind + '''
+                        border-1
+                    '''
+                else:
+                    form.fields[field_name].widget.attrs['class'] = tailwind + '''
+                        border-t-0 border-r-0 border-b-1 border-l-0
+                    '''
 
         context['form'] = form
         context['form_method'] = form_method
+        context['enctype'] = enctype
         return context
