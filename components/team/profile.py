877e35eb9d7897dd513f061a816b2f39608e9eb1
Joseph Muller <j.muller@bbk.ac.uk>
2023-10-25 09:06:22 +0100

    Add title and codename slots #159

--- components/team/profile.py
+++ components/team/profile.py
@@ -8,13 +8,11 @@ class TeamProfile(component.Component):
 
     def get_context_data(self, *args, **kwargs):
         name = kwargs.pop('name', '')
-        codename = kwargs.pop('codename', '')
         image_url = kwargs.pop('image_url', '')
         publications = kwargs.pop('publications', True)
         social_links = kwargs.pop('social_links', True)
         context = super().get_context_data(*args, **kwargs)
         context['name'] = name
-        context['codename'] = codename
         context['image_url'] = image_url
         context['publications'] = publications
         context['social_links'] = social_links
