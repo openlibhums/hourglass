af2304a2c162cc9a83025f2e5fd73b4c8640d908
Joseph Muller <j.muller@bbk.ac.uk>
2024-01-12 16:35:38 +0000

    Page regions for Open Access Movement #75

---
+++ components/timeline/section.py
@@ -0,0 +1,22 @@
+from django.template.utils import ImproperlyConfigured
+from django_components import component
+
+
+@component.register("timeline_section")
+class TimelineSection(component.Component):
+
+    def get_template_name(self, context):
+        try:
+            if context['alternate'] == 'odd':
+                return "timeline/section-odd.html"
+            elif context['alternate'] == 'even':
+                return "timeline/section-even.html"
+        except KeyError:
+            raise ImproperlyConfigured("Timeline section context must have alternate")
+
+    def get_context_data(self, alternate, *args, **kwargs):
+        year = kwargs.pop('year', '')
+        context = super().get_context_data(*args, **kwargs)
+        context['alternate'] = alternate
+        context['year'] = year
+        return context
