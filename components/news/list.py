ede26d0e082a7e9d7d77d2bdba7d67b240017728
Joseph Muller <j.muller@bbk.ac.uk>
2024-01-26 14:15:06 +0000

    Components for creating press overview #220

--- components/news/list.py
+++ components/news/list.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from django_components import component
-from comms import models
+
+from comms import models as comms_models
 
 
 @component.register("news_list")
@@ -7,7 +9,34 @@ class NewsList(component.Component):
 
     template_name = "news/list.html"
 
-    def get_context_data(self, h, *args, **kwargs):
+    def get_context_data(self, request, *args, **kwargs):
+        tag = kwargs.pop('tag', '')
+        content_type = kwargs.pop('content_type', '')
+        limit = kwargs.pop('limit', None)
+        h = kwargs.pop('h', '')
+        news_items = kwargs.pop('news_items', [])
         context = super().get_context_data(*args, **kwargs)
-        context['h'] = h
+
+        if not news_items:
+            if tag:
+                news_items = comms_models.NewsItem.active_objects.filter(
+                    content_type=request.model_content_type,
+                    object_id=request.site_type.id,
+                    tags__text=tag,
+                )
+            elif content_type and content_type in ['press', 'journal']:
+                news_items = comms_models.NewsItem.active_objects.filter(
+                    content_type__model=content_type,
+                )
+            else:
+                news_items = comms_models.NewsItem.active_objects.filter()
+
+        if limit:
+            news_items = news_items[:limit]
+
+        context = {
+            'h': h,
+            'news_items': news_items,
+        }
+
         return context
