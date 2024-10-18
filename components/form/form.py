e9a4b33331faf425f1f806800bfd3e4daf5223b2
Joseph Muller <j.muller@bbk.ac.uk>
2024-10-18 12:16:10 +0100

    Option to hide help text for required fields

--- components/form/form.py
+++ components/form/form.py
@@ -10,10 +10,12 @@ class Form(form_base.FormBase):
     def get_context_data(self, *args, **kwargs):
         form_action = kwargs.pop('form_action', '')
         form_method = kwargs.pop('form_method', '')
+        notify_required = kwargs.pop('notify_required', True)
         enctype = kwargs.pop('enctype', '')
         context = super().get_context_data(*args, **kwargs)
 
         context['form_method'] = form_method
         context['form_action'] = form_action
+        context['notify_required'] = notify_required
         context['enctype'] = enctype
         return context
