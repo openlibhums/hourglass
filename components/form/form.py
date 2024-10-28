e7cb505066f50d3a3d50e8540e13dbbbd03b1b25
Joseph Muller <j.muller@bbk.ac.uk>
2024-10-28 18:28:20 +0000

    Upgrade button components

--- components/form/form.py
+++ components/form/form.py
@@ -11,11 +11,14 @@ class Form(form_base.FormBase):
         form_action = kwargs.pop('form_action', '')
         form_method = kwargs.pop('form_method', '')
         notify_required = kwargs.pop('notify_required', True)
+        default_button = kwargs.pop('default_button', True)
         enctype = kwargs.pop('enctype', '')
         context = super().get_context_data(*args, **kwargs)
 
         context['form_method'] = form_method
         context['form_action'] = form_action
         context['notify_required'] = notify_required
+        context['default_button'] = default_button
         context['enctype'] = enctype
+
         return context
