d95ffb973b755f260dedb2f452c86edd33b8ec85
Joseph Muller <j.muller@bbk.ac.uk>
2024-10-28 18:42:40 +0000

    Make timeline component more flexible

--- components/timeline/section.py
+++ components/timeline/section.py
@@ -15,8 +15,10 @@ class TimelineSection(component.Component):
             raise ImproperlyConfigured("Timeline section context must have alternate")
 
     def get_context_data(self, alternate, *args, **kwargs):
-        year = kwargs.pop('year', '')
+        step = kwargs.pop('step', '')
+        bg = kwargs.pop('bg', 'bg-tan')
         context = super().get_context_data(*args, **kwargs)
         context['alternate'] = alternate
-        context['year'] = year
+        context['step'] = step
+        context['bg'] = bg
         return context
